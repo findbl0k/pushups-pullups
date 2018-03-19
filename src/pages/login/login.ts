4003688df02908ccd435f19f16d42293fbb44f5b
findbl0k <findbl0k@gmx.com>
2018-03-19 12:26:49 -0700

    add webstorm project

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,10 @@ import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
 
+import * as OktaAuth from '@okta/okta-auth-js';
+import { TabsPage } from '../tabs/tabs';
+
+
 @IonicPage()
 @Component({
   selector: 'page-login',
@@ -31,4 +35,44 @@ export class LoginPage {
       this.email.setFocus();
     }, 500);
   }
+
+  login(): void {
+    this.oauthService.createAndSaveNonce().then(nonce => {
+      const authClient = new OktaAuth({
+        clientId: this.oauthService.clientId,
+        redirectUri: this.oauthService.redirectUri,
+        url: 'https://{yourOktaDomain}.oktapreview.com',
+        issuer: 'default'
+      });
+      return authClient.signIn({
+        username: this.username,
+        password: this.password
+      }).then((response) => {
+        if (response.status === 'SUCCESS') {
+          return authClient.token.getWithoutPrompt({
+            nonce: nonce,
+            responseType: ['id_token', 'token'],
+            sessionToken: response.sessionToken,
+            scopes: this.oauthService.scope.split(' ')
+          })
+            .then((tokens) => {
+              const idToken = tokens[0].idToken;
+              const accessToken = tokens[1].accessToken;
+              const keyValuePair = `#id_token=${encodeURIComponent(idToken)}&access_token=${encodeURIComponent(accessToken)}`;
+              this.oauthService.tryLogin({
+                customHashFragment: keyValuePair,
+                disableOAuth2StateCheck: true
+              });
+              this.navCtrl.push(TabsPage);
+            });
+        } else {
+          throw new Error('We cannot handle the ' + response.status + ' status');
+        }
+      }).fail((error) => {
+        console.error(error);
+        this.error = error.message;
+      });
+    });
+  }
+
 }
