ea2ed0a9cfcff8d10112fca7f994fd69f73e9bbb
unknown <findbl0k@gmx.com>
2018-04-23 14:33:16 -0700

    update page names

--- src/pages/charts/charts.ts
+++ src/pages/charts/charts.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-charts',
+  templateUrl: 'charts.html'
 })
-export class ContactPage {
+export class ChartsPage {
 
   constructor(public navCtrl: NavController) {
 
