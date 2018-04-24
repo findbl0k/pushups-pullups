c7c3c431a447be950123813351b3641e077e2e7b
unknown <findbl0k@gmx.com>
2018-04-24 11:13:50 -0700

    add data entry placeholder functions

--- src/pages/entry/entry.ts
+++ src/pages/entry/entry.ts
@@ -14,16 +14,30 @@ export class EntryPage {
 
   }
 
-  testTag() {
+  setTag(tag, value) { //tag must be string, value is num or string
     this.platform.ready().then(() => {
-      window["plugins"].OneSignal.sendTag("test", 1);
+      window["plugins"].OneSignal.sendTag(tag, value);
     });
   }
 
-  buttonClick() {
+  createActivity() { //creates a new activity
 
-    this.testTag();
-    alert("test set");
+  }
+
+  destroyActivity() {//WARNING: PERMANENTLY DESTROYS AN ACTIVITY AND ALL DATA
+
+  }
+
+  updateToday() { //updates local storage and observable with user values
+
+
+    this.updateTags();
+  }
+
+  updateTags() { //runs checks and updates onesignal data tags
+
+    this.setTag("test", 1);
+    alert("test tag set");
   }
 
 }
