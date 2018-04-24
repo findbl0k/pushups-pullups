019673361c260196b2515bf3b6013abaa8df7f34
unknown <findbl0k@gmx.com>
2018-04-24 10:15:54 -0700

    add DataService

---
+++ src/services/dataService.ts
@@ -0,0 +1,54 @@
+import { Injectable, NgZone } from '@angular/core';
+import { Http} from "@angular/http";
+import { BehaviorSubject } from 'rxjs/BehaviorSubject'
+
+import 'rxjs/add/operator/finally';
+
+@Injectable()
+export class DataService {
+
+  userInfo: any;
+  userWorkouts: any;
+
+  public dataService = new BehaviorSubject(null);
+
+  constructor(public http: Http) {
+
+    this.userInfo = this.getStorageVariable('user_info');
+    this.userWorkouts = this.getStorageVariable('user_workouts');
+  }
+
+  static getStorageVariable(name) {
+    return JSON.parse(window.localStorage.getItem(name));
+  }
+
+  static setStorageVariable(name, data) {
+    window.localStorage.setItem(name, JSON.stringify(data));
+  }
+
+  private setUserInfo(payload) {
+    this.setStorageVariable('user_info', payload);
+  }
+
+  private setUserWorkouts(payload) {
+    this.setStorageVariable('user_workouts', payload);
+  }
+
+  public login() {
+
+      this.setUserInfo(null);
+      this.setUserWorkouts(null);
+  }
+
+  public destroy() {
+    //WARNING: TRIGGERING THIS FUNCTION DESTROYS ALL LOCAL DATA!
+    window.localStorage.removeItem('user_info');
+    window.localStorage.removeItem('user_workouts');
+
+    //delete observable data
+    this.dataService.next(null);
+
+    this.userInfo = null;
+    this.userWorkouts = null;
+  }
+}
